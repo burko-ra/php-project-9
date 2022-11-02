f64277675d4018aaa8135216ab8c0ed9d6af9ea3
Roman Burko <redstone.kem@gmail.com>
2022-11-02 13:35:58 +0300

    deleted test file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$url = 'HTTPS://GOOGLE.COM';
-$urlParts = parse_url(strtolower($url));
-$scheme = $urlParts['scheme'];
-$host = $urlParts['host'];
-$normalizedUrl = $scheme . $host;
-print($normalizedUrl);
-
