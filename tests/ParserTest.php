dc66690f4eb94801d0a26e52d93ecb761a9bcdb8
Roman Burko <redstone.kem@gmail.com>
2022-11-03 14:13:24 +0300

    create test file for Parser class

---
+++ tests/ParserTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PageAnalyzer\Parser;
+
+class ParserTest extends TestCase
+{
+    protected $client;
+    protected $code;
+
+    public function setUp(): void
+    {
+        $code = 200;
+
+        $stub = $this->createMock(\GuzzleHttp\Client::class);
+        $stub->method('get')
+            ->willReturn($this->returnSelf());
+        $stub->method('getStatusCode')
+        ->will($code);
+
+        $this->code = $code;
+
+        $this->client = $stub;
+    }
+
+    public function testGetStatusCode(): void
+    {
+        $parser = new Parser($this->client);
+        $this->assertEquals($this->code, $parser->getStatusCode('https://stub'));
+    }
+}
