54710f9797e9299072c9e6854215a3402591637e
Roman Burko <redstone.kem@gmail.com>
2022-11-15 12:17:22 +0300

    replaced hardcoded links with routes

--- public/index.php
+++ public/index.php
@@ -50,10 +50,11 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 /**
  * @var Container $this
  */
-$app->get('/', function ($request, $response) {
+$app->get('/', function ($request, $response) use ($router) {
     $params = [
         'url' => ['name' => ''],
-        'errors' => []
+        'errors' => [],
+        'router' => $router
     ];
     return $this->get('renderer')->render($response, 'index.phtml', $params);
 })->setName('root');
@@ -71,7 +72,8 @@ $app->post('/urls', function ($request, $response) use ($router) {
     if (is_array($errors) && count($errors) > 0) {
         $params = [
             'url' => ['name' => $urlName],
-            'errors' => array_slice($errors, 0, 1)
+            'errors' => array_slice($errors, 0, 1),
+            'router' => $router
         ];
         return $this->get('renderer')->render($response->withStatus(422), 'index.phtml', $params);
     }
@@ -93,9 +95,9 @@ $app->post('/urls', function ($request, $response) use ($router) {
     }
     $id = (string) $newUrl['id'];
     return $response->withRedirect($router->urlFor('url', ['id' => $id]), 302);
-});
+})->setName('urlsPost');
 
-$app->get('/urls/{id}', function ($request, $response, array $args) {
+$app->get('/urls/{id}', function ($request, $response, array $args) use ($router) {
     $flash = $this->get('flash')->getMessages();
     $urlId = htmlspecialchars($args['id']);
     $url = $this->get('urlRepo')->getById($urlId);
@@ -103,14 +105,18 @@ $app->get('/urls/{id}', function ($request, $response, array $args) {
     $params = [
         'url' => $url,
         'urlChecks' => $urlChecks,
-        'flash' => $flash
+        'flash' => $flash,
+        'router' => $router
     ];
     return $this->get('renderer')->render($response, 'urls/show.phtml', $params);
 })->setName('url');
 
-$app->get('/urls', function ($request, $response) {
+$app->get('/urls', function ($request, $response) use ($router) {
     $urls = $this->get('urlRepo')->all();
-    $params = ['urls' => $urls];
+    $params = [
+        'urls' => $urls,
+        'router' => $router
+    ];
     return $this->get('renderer')->render($response, 'urls/index.phtml', $params);
 })->setName('urls');
 
@@ -148,6 +154,6 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, array $args) 
     }
 
     return $response->withRedirect($router->urlFor('url', ['id' => $urlId]), 302);
-});
+})->setName('checksPost');
 
 $app->run();
